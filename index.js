6de1336144cd6bdc4072cc55b5ce5322528cf8c2
junn-era <junnera@gmail.com>
2023-11-16 10:52:11 -0300

    upload of actual code

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+let numberOfWins = 300
+let numberOfDefeats = 9
+let result = calcLevel(numberOfWins, numberOfDefeats);
 
-function rankedMatchesCalc(numberOfWins, playerDefeats){
-    let wins = 
+function calcLevel(numberOfWins, numberOfDefeats){
+  const balance = numberOfWins - numberOfDefeats;
+  let level;
+
+  if (numberOfWins < 10) {
+    level = "FERRO";
+  } else if (numberOfWins >= 10 && numberOfWins <= 20) {
+    level = "BRONZE";
+  } else if (numberOfWins >= 21 && numberOfWins <= 50) {
+    level = "PRATA"
+  } else if (numberOfWins >= 51 && numberOfWins <= 80) {
+    level = "Ouro"
+  } else if (numberOfWins >= 81 && numberOfWins <= 90) {
+    level = "Diamante"
+  } else if (numberOfWins >= 91 && numberOfWins <= 100) {
+    level = "LENDÁRIO"
+  } else {
+    level = "IMORTAL"
+  }
+  return `O Herói tem saldo de ${balance} e está no nível de ${level}`;
 }
+console.log(result);
